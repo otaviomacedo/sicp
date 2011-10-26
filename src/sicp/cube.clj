964fd34a3af303be2401ff3078dabc2b22ea0824
Otavio Augusto Cardoso Macedo <04714835670@serpro-1529733.spo.serpro>
2011-10-26 16:45:53 -0200

    Made some functions private to their respective namespaces.

--- src/sicp/cube.clj
+++ src/sicp/cube.clj
@@ -2,10 +2,10 @@
   (:use (clojure.contrib math))
   (:use sicp.main))
 
-(defn improve [guess n]
+(defn- improve [guess n]
   (/ (+ (/ n (square guess)) (* 2 guess)) 3))
 
-(defn good-enough? [guess n]
+(defn- good-enough? [guess n]
   (<
     (abs (- (improve guess n) guess))
     (abs (* guess 0.001))))
--- src/sicp/square.clj
+++ src/sicp/square.clj
@@ -2,10 +2,10 @@
   (:use (clojure.contrib math))
   (:use sicp.main))
 
-(defn improve [guess n]
+(defn- improve [guess n]
   (/ (+ (/ n guess) guess) 2))
 
-(defn good-enough? [guess n]
+(defn- good-enough? [guess n]
   (< (abs (- (improve guess n) guess))
     (abs (* guess 0.0001))))
 
