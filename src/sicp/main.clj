aca3cd9a4edbdee6b6a25505564d1e66694af177
Otavio Macedo <otavio@ventania.(none)>
2011-10-28 09:30:31 -0200

    Factorial and Ackermann functions.

--- src/sicp/main.clj
+++ src/sicp/main.clj
@@ -11,3 +11,19 @@
 
 (defn cube [n]
   (* n (square n)))
+
+(defn factorial
+  ([n] (factorial (dec n) n n))
+  ([counter product n]
+    (if (= counter 1)
+      product
+      (recur (dec counter) (* product counter) n))))
+
+(defn A [x y]
+  (cond (= y 0) 0
+        (= x 0) (* 2 y)
+        (= y 1) 2
+        :else (A (- x 1) (A x (- y 1)))))
+
+(print (A 1 4))
+
