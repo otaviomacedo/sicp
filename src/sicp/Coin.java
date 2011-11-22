0d057155f8e1f57fce8b31c39ae6026a5cf3fc55
Otavio Augusto Cardoso Macedo <04714835670@serpro-1529733.spo.serpro>
2011-11-22 18:00:25 -0200

    Experimental Java version.

---
+++ src/sicp/Coin.java
@@ -0,0 +1,60 @@
+package sicp;
+
+
+import java.util.*;
+
+public class Coin {
+
+    public static void main(String[] args) {
+        int amount = 29;
+        List<Integer> coins = Arrays.asList(1, 5, 10, 25, 50);
+
+        Map<Integer, Integer> weights = weights(amount, coins);
+
+        int result = 0;
+        for (int i = 1; i < coins.size(); i++){
+            Iterator<Integer> multiples = multiples(amount, coins.get(i));
+            int weight = amount / coins.get(i);
+            while (multiples.hasNext()){
+                Integer mult = multiples.next();
+                if (weights.containsKey(mult)){
+                    int newWeight = weight + weights.get(mult);
+                    weight--;
+                    result += weights.get(mult);
+                    weights.put(mult, newWeight);
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+
+    private static Map<Integer, Integer> weights(int amount, List<Integer> coins) {
+        Map<Integer, Integer> weights = new HashMap<Integer, Integer>();
+        Iterator<Integer> multiples = multiples(amount, coins.get(0));
+        while (multiples.hasNext()){
+            weights.put(multiples.next(), 1);
+        }
+        return weights;
+    }
+
+    private static Iterator<Integer> multiples(final int amount, final int coin){
+        return new Iterator<Integer>() {
+            int nextValue = amount - coin;
+            public boolean hasNext() {
+                return nextValue >= 0;
+            }
+
+            public Integer next() {
+                int result = nextValue;
+                nextValue -= coin;
+                return result;
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+}
