a9be41f1e4687f15d4c98e65a52be938e07cae1d
Otavio Augusto Cardoso Macedo <04714835670@serpro-1529733.spo.serpro>
2011-11-24 17:14:51 -0200

    Experimental Java version.

--- src/sicp/Coin.java
+++ src/sicp/Coin.java
@@ -2,29 +2,70 @@ package sicp;
 
 import java.util.*;
 
+import static com.google.common.collect.Maps.newHashMap;
+
 public class Coin {
 
     public static void main(String[] args) {
-        int amount = 49;
+        int amount = 50;
         List<Integer> coins = Arrays.asList(1, 5, 10, 25, 50);
 
-        Map<Integer, Integer> weights = weights(amount, coins);
+        Map<Integer, Node> nodes = nodes(amount, coins);
 
-        int result = 1;
+        int result = 0;
         for (int i = 1; i < coins.size(); i++){
             Iterator<Integer> multiples = multiples(amount, coins.get(i));
-            int weight = amount / coins.get(i);
+            Node node = new Node(0, i);
             while (multiples.hasNext()){
                 Integer mult = multiples.next();
-                if (weights.containsKey(mult)){
-                    result += weights.get(mult);
-                    weights.put(mult, weight--);
+                if (nodes.containsKey(mult)){
+                    node = nodes.get(mult).shift(i, coins).plus(node);
+                    result = node.degree;
+                    nodes.put(mult, node);
                 }
             }
 
         }
         
-        System.out.println(result);
+        System.out.println(result + 1);
+    }
+
+
+    private static class Node {
+        private final int degree, level;
+
+        Node(int degree, int level) {
+            this.degree = degree;
+            this.level = level;
+        }
+
+        Node shift(int level, List<Integer> coins){
+            return level - this.level == 1
+                    ? new Node(degree, level)
+                    : new Node(degree + coins.get(this.level), level);
+        }
+
+        Node plus(Node other){
+            return new Node(this.degree + other.degree, level);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", degree, level);
+        }
+    }
+
+    private static Map<Integer, Node> nodes(int amount, List<Integer> coins){
+        Map<Integer, Node> nodes = newHashMap();
+
+        Iterator<Integer> multiples = multiples(amount, coins.get(0));
+
+        final Node baseNode = new Node(1, 0);
+        while (multiples.hasNext()){
+            nodes.put(multiples.next(), baseNode);
+        }
+
+        return nodes;
     }
 
     private static Map<Integer, Integer> weights(int amount, List<Integer> coins) {
@@ -38,14 +79,15 @@ public class Coin {
 
     private static Iterator<Integer> multiples(final int amount, final int coin){
         return new Iterator<Integer>() {
-            int nextValue = amount - coin;
+            int nextValue = amount % coin;
+
             public boolean hasNext() {
-                return nextValue >= 0;
+                return nextValue < amount;
             }
 
             public Integer next() {
                 int result = nextValue;
-                nextValue -= coin;
+                nextValue += coin;
                 return result;
             }
 
