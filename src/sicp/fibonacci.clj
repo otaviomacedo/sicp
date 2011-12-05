6882145a2775c12ff1d359e0deb300609313e334
Otavio Augusto Cardoso Macedo <04714835670@serpro-1529733.spo.serpro>
2011-12-05 12:09:14 -0200

    Fibonacci algorithm with O(log n).

--- src/sicp/fibonacci.clj
+++ src/sicp/fibonacci.clj
@@ -5,3 +5,46 @@
     (if (= counter n)
       a
       (recur b (+' a b) (inc counter)))))
+
+(defn square [n] (*' n n))
+
+(def primitives {0 0, 1 1, 2 1})
+
+(defn sum-of-squares [a b]
+  (+' (square a) (square b)))
+
+;TODO rename
+(defn function-a [lesser greater]
+  (*' greater (+ (* 2 lesser) greater)))
+
+;TODO rename
+(defn function-c [lesser greater]
+  (*' lesser (- (* 2 greater) lesser)))
+
+(defn half [n]
+  (if (even? n)
+    (/ n 2)
+    (/ (inc n) 2)))
+
+(defn make-stack [n]
+  (loop [stack (list) head n]
+    (if (= 2 head)
+      stack
+      (recur (cons head stack) (half head)))))
+
+(defn fib-stack [lesser greater stack]
+  (cond
+    (empty? stack) greater
+    (even? (first stack)) (recur (sum-of-squares lesser greater) (function-a lesser greater) (rest stack))
+    :else (recur (function-c lesser greater) (sum-of-squares lesser greater) (rest stack))))
+
+(defn fib [n]
+  (def candidate (primitives n))
+  (if candidate
+    candidate
+    (fib-stack 1 1 (make-stack n))))
+
+(def n 10000)
+(println (time (fib n)))
+(println (time (fibonacci n)))
+
