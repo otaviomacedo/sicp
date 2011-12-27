1518afacd2852fd01e5f00cf942881a7206d9c4a
Otavio Augusto Cardoso Macedo <04714835670@serpro-1529733.spo.serpro>
2011-12-27 08:27:12 -0200

    Filtered accumulation implemented

--- src/sicp/hof.clj
+++ src/sicp/hof.clj
@@ -1,27 +1,25 @@
-(ns sicp.hof)
+(ns sicp.hof
+  (:use [sicp.primality]))
 
 (defn cube [n]
   (* n n n))
 
-(defn sum-iter [term a next b acc]
+(defn square [n] (* n n))
+
+(defn accumulation [term a next b acc op]
   (if (> a b)
     acc
-    (recur term (next a) next b (+ acc (term a)))))
+    (recur term (next a) next b (op acc (term a)) op)))
 
 (defn sum [term a next b]
-  (sum-iter term a next b 0))
+  (accumulation term a next b 0 +))
 
 (defn integral [f a b dx]
   (letfn [(add-dx [x] (+ x dx))]
     (* (sum f (+ a (/ dx 2.0)) add-dx b) dx)))
 
-(defn product-iter [term a next b acc]
-  (if (> a b)
-    acc
-    (recur term (next a) next b (*' acc (term a)))))
-
 (defn product [term a next b]
-  (product-iter term a next b 1))
+  (accumulation term a next b 1 *'))
 
 (defn simpsons-rule
   "Simpson's method for numerical function integration"
@@ -40,3 +38,14 @@
 
 (defn factorial [n]
   (product identity 1 inc n))
+
+
+(defn filtered-accumulate [term a next b acc op pred]
+  (def value (if (pred a) (op acc (term a)) acc))
+  (println a ", " value)
+  (if (> a b)
+    acc
+    (recur term (next a) next b value op pred)))
+
+(defn sum-of-squares-of-primes [a b]
+  (filtered-accumulate square a inc b 0 + prime?))
--- test/sicp/test/hof.clj
+++ test/sicp/test/hof.clj
@@ -17,3 +17,7 @@
   (it "calculates factorial for large numbers"
     (= 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
       (factorial 100))))
+
+(describe "Accumulation with filtering"
+  (it "sums the squares of the prime numbers in a given interval"
+    (= 1014 (sum-of-squares-of-primes 5 20))))
