43030d97e44fe3558e27596d6bfde98db319825f
Otavio Macedo <otaviomacedo@gmail.com>
2012-06-28 16:49:22 -0300

    Taking the work up again, after a long pause.

--- src/sicp/change.clj
+++ src/sicp/change.clj
@@ -1,33 +1,47 @@
 (ns sicp.change
   (:use [clojure.set]))
 
-(def value (hash-map
-         5, 50,
-         4, 25,
-         3, 10,
-         2, 5,
-         1, 1))
+(def values '(1, 5, 10, 25, 50))
 
-(defn rec-sum [fn amount coin i acc]
-  (if (> i (/ amount (get value coin)))
-    acc
-    (fn (- amount (* i (get value coin))) (dec coin))))
+(defn value-of [coin]
+  (nth values coin))
 
-(defn change-count [amount coin]
-  (if ((or (= amount 0) (= coin 1)))
-    1
-    (rec-sum change-count amount coin 0 0)))
+(defn change-count-recursive [amount coin]
+  (cond
+    (neg? amount) 0
+    (or (zero? amount) (zero? coin)) 1
+    :else (+
+            (change-count-recursive (- amount (value-of coin)) coin)
+            (change-count-recursive amount (dec coin)))))
 
-
-;private static int f(int a, int c, int level) {
-;                                                if (a == 0 || c == 1) {
-;                                                                        return 1;
-;                                                                        }
+;    private static int changeCoinsLinearSpace(int amount, int coin){
+;        List<Integer> combination = newArrayList(Collections.nCopies(coin - 1, 0))
 ;
-;  int result = 0;
-;  for (int i = 0; i <= a / V.get(c); i++) {
-;        result +=  f(a - i*V.get(c), c -1, level + 1);
+;        int result = 0;
+;        while (carry(combination, amount)){
+;            result++;
 ;        }
+;        return result + 1;
+;    }
 ;
-;        return result;
+;    private static boolean carry(List<Integer> combination, int amount){
+;        int i = 0;
+;        combination.set(i, combination.get(i) + 1);
+;        while (overflow(combination, amount)){
+;            combination.set(i, 0);
+;            i++;
+;            if (i > combination.size() - 1){
+;                return false;
+;            }
+;            combination.set(i, combination.get(i) + 1);
 ;        }
+;        return true;
+;    }
+
+
+
+(defn overflow [combination amount]
+  (> (reduce + (map * combination (rest values))) amount))
+
+
+(println "Resultado: " (change-count-recursive 100 4))
